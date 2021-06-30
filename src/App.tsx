66eb4588bc318097eabbf14912214f5d5d7b1cc1
Noah Betzen <noah@nezteb.net>
2021-06-30 17:03:57 -0500

    Fix filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React, { useMemo, useState } from 'react'
-import { useBlockLayout, usePagination, useResizeColumns, useSortBy, useTable } from 'react-table'
+import {
+  useBlockLayout,
+  usePagination,
+  useResizeColumns,
+  useSortBy,
+  useTable,
+  useFilters,
+  useGlobalFilter,
+  useAsyncDebounce
+} from 'react-table'
 import styled from 'styled-components'
+import { matchSorter } from 'match-sorter'
 import stars from '../scripts/stars.json';
+import "regenerator-runtime/runtime.js";
 
 type Star = {
   starred_at: string,
@@ -20,43 +31,87 @@ function App() {
   const columns = useMemo(() => [
     {
       Header: "Name",
-      accessor: "fullname"
+      accessor: "fullname",
+      filter: 'fuzzyText'
     },
     {
       Header: "Description",
-      accessor: "description"
+      accessor: "description",
+      filter: 'fuzzyText'
     },
     {
       Header: "Language",
-      accessor: "language"
+      accessor: "language",
+      filter: 'fuzzyText'
     },
     {
       Header: "Labels",
-      accessor: "labels"
+      accessor: "labels",
+      filter: 'fuzzyText'
     },
     {
       Header: "Stars",
-      accessor: "stargazers_count"
+      accessor: "stargazers_count",
+      Filter: NumberRangeColumnFilter,
+      filter: 'between',
     },
     {
       Header: "URL",
-      accessor: "html_url"
+      accessor: "html_url",
+      filter: 'fuzzyText'
     },
     {
       Header: "Homepage",
-      accessor: "homepage"
+      accessor: "homepage",
+      filter: 'fuzzyText'
     },
     {
       Header: "Starred At",
-      accessor: "starred_at"
+      accessor: "starred_at",
+      filter: 'fuzzyText'
     }
   ], []);
 
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+              .toLowerCase()
+              .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
   const tableInstance = useTable(
-    { columns, data },
-    useSortBy,
+    {
+      columns,
+      data,
+      defaultColumn,
+      filterTypes
+    },
     useBlockLayout,
     useResizeColumns,
+    useFilters,
+    useGlobalFilter,
+    useSortBy,
     usePagination
   )
 
@@ -65,7 +120,7 @@ function App() {
     getTableBodyProps,
     headerGroups,
     prepareRow,
-    //rows,
+    // Paging
     page,
     canPreviousPage,
     canNextPage,
@@ -75,7 +130,10 @@ function App() {
     nextPage,
     previousPage,
     setPageSize,
-    state: { pageIndex, pageSize },
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
   } = tableInstance
 
   return (
@@ -100,23 +158,22 @@ function App() {
         <span>
           Page{' '}
           <strong>
-            {pageIndex + 1} of {pageOptions.length}
+            {state.pageIndex + 1} of {pageOptions.length}
           </strong>{' '}
         </span>
         <span>
           | Go to page:{' '}
           <input
             type="number"
-            defaultValue={pageIndex + 1}
+            defaultValue={state.pageIndex + 1}
             onChange={e => {
               const page = e.target.value ? Number(e.target.value) - 1 : 0
               gotoPage(page)
             }}
-            style={{ width: '100px' }}
           />
         </span>{' '}
         <select
-          value={pageSize}
+          value={state.pageSize}
           onChange={e => {
             setPageSize(Number(e.target.value))
           }}
@@ -142,6 +199,7 @@ function App() {
                       <th {...column.getHeaderProps(column.getSortByToggleProps())} className="th">
                         {// Render the header
                           column.render('Header')}
+                        <div>{column.canFilter ? column.render('Filter') : null}</div>
                         <span>
                           {column.isSorted
                             ? column.isSortedDesc
@@ -158,6 +216,20 @@ function App() {
                     ))}
                 </tr>
               ))}
+            <tr>
+              <th
+                colSpan={visibleColumns.length}
+                style={{
+                  textAlign: 'left',
+                }}
+              >
+                <GlobalFilter
+                  preGlobalFilteredRows={preGlobalFilteredRows}
+                  globalFilter={state.globalFilter}
+                  setGlobalFilter={setGlobalFilter}
+                />
+              </th>
+            </tr>
           </thead>
           {/* Apply the table body props */}
           <tbody {...getTableBodyProps()}>
@@ -186,6 +258,158 @@ function App() {
     </Styles>
   )
 }
+
+// Define a custom filter filter function!
+function filterGreaterThan(rows, id, filterValue) {
+  return rows.filter(row => {
+    const rowValue = row.values[id]
+    return rowValue >= filterValue
+  })
+}
+
+// This is an autoRemove method on the filter function that
+// when given the new filter value and returns true, the filter
+// will be automatically removed. Normally this is just an undefined
+// check, but here, we want to remove the filter if it's not a number
+filterGreaterThan.autoRemove = val => typeof val !== 'number'
+
+// Define a default UI for filtering
+function GlobalFilter({
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+      />
+    </span>
+  )
+}
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+      onClick={(e) => { e.preventDefault(); e.stopPropagation() }}
+    />
+  )
+}
+
+// This is a custom filter UI for selecting
+// a unique option from a list
+function SelectColumnFilter({
+  column: { filterValue, setFilter, preFilteredRows, id },
+}) {
+  // Calculate the options for filtering
+  // using the preFilteredRows
+  const options = React.useMemo(() => {
+    const options = new Set()
+    preFilteredRows.forEach(row => {
+      options.add(row.values[id])
+    })
+    return [...options.values()]
+  }, [id, preFilteredRows])
+
+  // Render a multi-select box
+  return (
+    <select
+      value={filterValue}
+      onChange={e => {
+        setFilter(e.target.value || undefined)
+      }}
+    >
+      <option value="">All</option>
+      {options.map((option, i) => (
+        <option key={i} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+// This is a custom UI for our 'between' or number range
+// filter. It uses two number boxes and filters rows to
+// ones that have values between the two
+function NumberRangeColumnFilter({
+  column: { filterValue = [], preFilteredRows, setFilter, id },
+}) {
+  const [min, max] = React.useMemo(() => {
+    let min = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    let max = preFilteredRows.length ? preFilteredRows[0].values[id] : 0
+    preFilteredRows.forEach(row => {
+      min = Math.min(row.values[id], min)
+      max = Math.max(row.values[id], max)
+    })
+    return [min, max]
+  }, [id, preFilteredRows])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <input
+        value={filterValue[0] || ''}
+        type="number"
+        onChange={e => {
+          const val = e.target.value
+          setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
+        }}
+        placeholder={`Min (${min})`}
+        style={{
+          width: '100%',
+        }}
+        onClick={(e) => { e.preventDefault(); e.stopPropagation() }}
+      />
+      to
+      <input
+        value={filterValue[1] || ''}
+        type="number"
+        onChange={e => {
+          const val = e.target.value
+          setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
+        }}
+        placeholder={`Max (${max})`}
+        style={{
+          width: '100%',
+        }}
+        onClick={(e) => { e.preventDefault(); e.stopPropagation() }}
+      />
+    </div>
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
 
 const Styles = styled.div`
   padding: 1rem;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import babel from 'vite-babel-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    babel()
+  ]
 })
